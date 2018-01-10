b967db5fed141fa0dec0243bffb142699f973eac
Jamie Chu <huanjamierose@gmail.com>
2018-01-10 04:46:51 -0500

    first commit

--- ObjC_Proj/ViewController.h
+++ ObjC_Proj/ViewController.h
@@ -8,8 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+//interface and implementations both closed with an @end
+
 @interface ViewController : UIViewController
 
+// syntax for declared properties: @property (<#attributes#>) <#type#> <#name#>;
+//retain means increase the retain count by 1 and then take ownership of the object
+//* means you are pointer to a car rather than
+//nonatomic means that you
+@property (retain, nonatomic) UILabel *myLabel;
+
+
+@property (weak, nonatomic) IBOutlet UITextField  *myTextField;
 
 @end
 
